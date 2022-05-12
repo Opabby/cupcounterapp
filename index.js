58a40e6787c24a9cc7933637f9db5e734ea2d30d
Thaís Rodeiro <ttheinterpreter@gmail.com>
2022-05-12 18:32:47 -0300

    Function save()

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 let count = 0;
-let countEl = document.getElementById("count-el");
+let countEl = document.getElementById("count-el"); //to pas in arguments
+let saveEl = document.getElementById("save-el");
 
 function increment(){
     count++;
-    countEl.innertext = count;
-    console.log(count);
+    countEl.textContent = count;
+}
+
+function save() {
+    let save = count + " - ";
+    saveEl.textContent += save;
+    countEl.textContent = 0;
+    count = 0;
 }
 
