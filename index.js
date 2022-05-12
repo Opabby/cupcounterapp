f8fb7d52567b8a66850b8547ed19347325154c57
Thaís Rodeiro <ttheinterpreter@gmail.com>
2022-05-12 17:06:11 -0300

    Adding count-el variable

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 let count = 0;
+let countEl = document.getElementById("count-el");
 
 function increment(){
     count++;
-    document.getElementById("count-el").innerText = count;
+    countEl.innertext = count;
     console.log(count);
 }
 
